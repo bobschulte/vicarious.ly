42fd5bf974e6092c1331fd8f995ffc6611bdc449
Robert Schulte <schulte.robertw@outlook.com>
2019-01-09 18:40:01 -0600

    more postgres config

--- backend/start.js
+++ backend/start.js
@@ -1,20 +1,20 @@
-// // replace line 2 with sequelize
-// const mongoose = require('mongoose')
-
 // import environment variables from variables.env
 require('dotenv').config({ path: 'variables.env' })
+
 
 // // connect to database and handle any bad connections
 // mongoose.connect(process.env.DATABASE);
 // mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
 // mongoose.connection.on('error', (err) => {
-//     console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
-// });
+    //     console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+    // });
+    
+// import db
+const db = require('./server/models/index')
 
-
-// // start the app!
-// const app = require('./app')
-// app.set('port', process.env.PORT || 7777)
-// const server = app.listen(app.get('port'), () => {
-//     console.log(`Express running → PORT ${server.address().port}`);
-// })
+// start the app!
+const app = require('./app')
+app.set('port', process.env.PORT || 7777)
+const server = app.listen(app.get('port'), () => {
+    console.log(`Express running → PORT ${server.address().port}`);
+})
