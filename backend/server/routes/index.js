2124d8a13edcf19e391319b5ae693cedb51d152b
Robert Schulte <schulte.robertw@outlook.com>
2019-01-10 02:27:58 -0600

    removed console logs

--- backend/server/routes/index.js
+++ backend/server/routes/index.js
@@ -4,19 +4,16 @@ const models = require('../models/index')
 
 
 router.get('/cities', function (req, res) {
-    console.log('cities route call made!')
     models.City.findAll({ include: models.Stay })
     .then(cities => res.json(cities))
 });
 
 router.get('/travelers', function (req, res) {
-    console.log('travelers route call made!')
     models.Traveler.findAll({ include: models.Stay })
     .then(travelers => res.json(travelers))
 });
 
 router.get('/stays', function (req, res) {
-    console.log('stays route call made!')
     models.Stay.findAll({ include: [ models.City, models.Traveler ] })
     .then(stays => res.json(stays))
 });
