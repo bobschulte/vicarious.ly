a8a848e021b973e2914a01666852a2d53514e51c
Robert Schulte <schulte.robertw@outlook.com>
2019-01-10 02:27:05 -0600

    remove console logs

--- backend/server/routes/index.js
+++ backend/server/routes/index.js
@@ -6,28 +6,19 @@ const models = require('../models/index')
 router.get('/cities', function (req, res) {
     console.log('cities route call made!')
     models.City.findAll({ include: models.Stay })
-    .then(cities => {
-        console.log(cities)
-        res.json(cities)
-    });
+    .then(cities => res.json(cities))
 });
 
 router.get('/travelers', function (req, res) {
     console.log('travelers route call made!')
     models.Traveler.findAll({ include: models.Stay })
-    .then(travelers => {
-        console.log(travelers)
-        res.json(travelers)
-    })
+    .then(travelers => res.json(travelers))
 });
 
 router.get('/stays', function (req, res) {
     console.log('stays route call made!')
     models.Stay.findAll({ include: [ models.City, models.Traveler ] })
-    .then(stays => {
-        console.log(stays)
-        res.json(stays)
-    });
+    .then(stays => res.json(stays))
 });
 
 module.exports = router
