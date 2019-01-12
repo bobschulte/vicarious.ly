83df32063758a0b2229a0c393324e66208491205
Robert Schulte <schulte.robertw@outlook.com>
2019-01-11 23:43:00 -0600

    refactored start.js using async await

--- backend/start.js
+++ backend/start.js
@@ -1,25 +1,21 @@
 const db = require('./models/index')
 const seed = require('./seeds/seed')
 
-db.sequelize.sync({ force: true }).then(() => {
-    // seed db tables
-    seed(db)
-    .catch(error => console.log(error, 'ERROR WHILE RE-SEEDING'))
-    .then(() => console.log('DATABASE RE-SEEDED'))
-    .then (() => {
-        // console.log playground
-        
-    })
-    .then(() => {
-        // start the app! (this should maybe go below this db sync function?)
-        const app = require('./app')
-        app.set('port', process.env.PORT || 7777)
-        const server = app.listen(app.get('port'), error => {
-            if (!error) {
-                console.log(`Express running → PORT ${server.address().port}`);
-            } else {
-                console.log(error, 'app launch didnt work')
-            }
-        })
+db.sequelize.sync({ force: true }).then(async () => {
+
+    // re-seed db
+    await seed(db)
+    console.log('DATABASE RE-SEEDED')
+
+    // start app
+    const app = require('./app')
+    app.set('port', process.env.PORT || 7777)
+    
+    const server = app.listen(app.get('port'), error => {
+        if (!error) {
+            console.log(`Express running → PORT ${server.address().port}`);
+        } else {
+            console.log(error, 'app launch didnt work')
+        }
     })
 })
