3cc05e12e8a2e1ddc22f15b6037f3bfdacfb8c8f
Robert Schulte <schulte.robertw@outlook.com>
2019-01-12 00:00:38 -0600

    app.js cleanup

--- backend/app.js
+++ backend/app.js
@@ -6,35 +6,22 @@ const expressValidator = require('express-validator')
 const passport = require('passport')
 require('./services/passport')
 const flash = require('connect-flash')
-const routes = require('./routes/index')
+const router = require('./routes/index')
 
-// create Express app
+
 const app = express()
 
-app.use(express.static('./public'));
-
-// enable cross-origin resource sharing
+// middleware
 app.use(cors())
-
-// takes the raw requests and turns them into usable properties on req.body
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// populates req.cookies with any cookies that came along with the request
 app.use(cookieParser());
-
-// enable data validation methods on 'req'
 app.use(expressValidator());
-
-// add passport to handle authentication
 app.use(passport.initialize())
 app.use(passport.session())
-
-// enable flash so we can use req.flash to pass messages back to user via the next page they request
 app.use(flash())
 
-// enable routes
-app.use('/', routes);
+app.use('/', router);
 
 // ERROR HANDLING
 
