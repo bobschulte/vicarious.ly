374a819b68c2e0f018313c9627e94ccad187d1c0
Robert Schulte <schulte.robertw@outlook.com>
2019-01-11 23:37:40 -0600

    cleaned up start.js script

--- backend/start.js
+++ backend/start.js
@@ -4,21 +4,22 @@ const seed = require('./seeds/seed')
 db.sequelize.sync({ force: true }).then(() => {
     // seed db tables
     seed(db)
-    .then(res => console.log('DATABASE RE-SEEDED'))
-    .then (res => {
+    .catch(error => console.log(error, 'ERROR WHILE RE-SEEDING'))
+    .then(() => console.log('DATABASE RE-SEEDED'))
+    .then (() => {
         // console.log playground
         
     })
-    .catch(error => console.log(error, 'error while re-seeding'))
-
-    // start the app! (this should maybe go below this db sync function?)
-    const app = require('./app')
-    app.set('port', process.env.PORT || 7777)
-    const server = app.listen(app.get('port'), error => {
-        if (!error) {
-            console.log(`Express running → PORT ${server.address().port}`);
-        } else {
-            console.log(error, 'app launch didnt work')
-        }
+    .then(() => {
+        // start the app! (this should maybe go below this db sync function?)
+        const app = require('./app')
+        app.set('port', process.env.PORT || 7777)
+        const server = app.listen(app.get('port'), error => {
+            if (!error) {
+                console.log(`Express running → PORT ${server.address().port}`);
+            } else {
+                console.log(error, 'app launch didnt work')
+            }
+        })
     })
 })
