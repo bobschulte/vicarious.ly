3cada0096ee57ed34c2c92f74c7f9f1a1d595d91
Robert Schulte <schulte.robertw@outlook.com>
2019-01-15 15:56:34 -0600

    navbar styling

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,26 +4,32 @@ import actions from '../state/actions/index'
 import AppBar from '@material-ui/core/AppBar'
 import ToolBar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
 import Button from '@material-ui/core/Button'
 
 class NavBar extends React.Component {
 
-    renderLogoutButton = () => {
-        if (localStorage.getItem('vicariouslyToken')) {
-            return <Button variant='contained' onClick={() => this.props.logoutUser()} > Logout </Button>
-        }
-    }
-
     render() {
-        return <div>
-            <ToolBar>
-              <AppBar>
-                <Typography variant="h4" color="inherit">
+        let token = localStorage.getItem("vicariouslyToken");
+
+        return <div style={{ flexgrow: 1 }}>
+            <AppBar position="static">
+              <ToolBar>
+                <IconButton style={{ marginLeft: -12, marginRight: 20 }} color='inherit' >
+                    <MenuIcon />
+                </IconButton>
+                <Typography style={{ flexgrow: 1, marginLeft: 10, marginRight: 10 }} variant="h4" color="inherit">
                   VICARIOUS.LY
                 </Typography>
-                {this.renderLogoutButton()}
-              </AppBar>
-            </ToolBar>
+                {token && <div>
+                    <Button style={{ marginLeft: 20, marginRight: 20 }} variant="contained" onClick={() => this.props.logoutUser()}>
+                      {" "}
+                      Logout{" "}
+                    </Button>
+                  </div>}
+              </ToolBar>
+            </AppBar>
           </div>;
     }
 }
