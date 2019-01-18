5040d90818721602ee55d131949f7351f074478f
Robert Schulte <schulte.robertw@outlook.com>
2019-01-18 15:27:21 -0600

    users, cities, stays seeded in db

--- backend/seeds/seed.js
+++ backend/seeds/seed.js
@@ -2,41 +2,41 @@ const seed = async (db) => {
     // db.User.destroy({ where: {} })
     // db.Stay.destroy({ where: {} })
 
-    let cities = await Promise.all([
-        db.City.findOne({ where: { name: 'Bogotá', country: 'Colombia' } }),
-        db.City.findOne({ where: { name: 'Bangkok', country: 'Thailand' } }),
-        db.City.findOne({ where: { name: 'London', country: 'United Kingdom' } }),
-        db.City.findOne({ where: { name: 'Madrid', country: 'Spain' } }),
-        db.City.findOne({ where: { name: 'Tokyo', country: 'Japan' } }),
-        db.City.findOne({ where: { name: 'Cape Town', country: 'South Africa' } }),
-        db.City.findOne({ where: { name: 'Buenos Aires', country: 'Argentina' } })
-    ])
+    // let cities = await Promise.all([
+    //     db.City.findOne({ where: { name: 'Bogotá', country: 'Colombia' } }),
+    //     db.City.findOne({ where: { name: 'Bangkok', country: 'Thailand' } }),
+    //     db.City.findOne({ where: { name: 'London', country: 'United Kingdom' } }),
+    //     db.City.findOne({ where: { name: 'Madrid', country: 'Spain' } }),
+    //     db.City.findOne({ where: { name: 'Tokyo', country: 'Japan' } }),
+    //     db.City.findOne({ where: { name: 'Cape Town', country: 'South Africa' } }),
+    //     db.City.findOne({ where: { name: 'Buenos Aires', country: 'Argentina' } })
+    // ])
     
-    const bogota = cities[0]
-    const bangkok = cities[1]
-    const london = cities[2]
-    const madrid = cities[3]
-    const tokyo = cities[4]
-    const johannesburg = cities[5]
-    const buenosAires = cities[6]
+    // const bogota = cities[0]
+    // const bangkok = cities[1]
+    // const london = cities[2]
+    // const madrid = cities[3]
+    // const tokyo = cities[4]
+    // const johannesburg = cities[5]
+    // const buenosAires = cities[6]
     
-    let users = await Promise.all([
-        db.User.findOne({ where: { firstName: 'Robert' } }),
-        db.User.findOne({ where: { firstName: 'Dawson' } }),
-        db.User.findOne({ where: { firstName: 'Milam' } }),
-        db.User.findOne({ where: { firstName: 'Charlie' } }),
-        db.User.findOne({ where: { firstName: 'Jackson' } }),
-        db.User.findOne({ where: { firstName: 'Wilson' } }),
-        db.User.findOne({ where: { firstName: 'Will' } })
-    ])
+    // let users = await Promise.all([
+    //     db.User.findOne({ where: { firstName: 'Robert' } }),
+    //     db.User.findOne({ where: { firstName: 'Dawson' } }),
+    //     db.User.findOne({ where: { firstName: 'Milam' } }),
+    //     db.User.findOne({ where: { firstName: 'Charlie' } }),
+    //     db.User.findOne({ where: { firstName: 'Jackson' } }),
+    //     db.User.findOne({ where: { firstName: 'Wilson' } }),
+    //     db.User.findOne({ where: { firstName: 'Will' } })
+    // ])
     
-    const robert = users[0]
-    const dawson = users[1]
-    const milam = users[2]
-    const charlie = users[3]
-    const jackson = users[4]
-    const will = users[5]
-    const wilson = users[6]
+    // const robert = users[0]
+    // const dawson = users[1]
+    // const milam = users[2]
+    // const charlie = users[3]
+    // const jackson = users[4]
+    // const will = users[5]
+    // const wilson = users[6]
     
     // await Promise.all([
     //     db.Stay.create( { UserId: robert.id, CityId: buenosAires.id, arrival: new Date(2018, 11, 24), departure: new Date(2019, 1, 4) } ),
